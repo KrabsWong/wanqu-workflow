e167fc32dc87743a9cfd5f54c2125704b08e6c3b
Mr.Krabs <pangxie.wdy@gmail.com>
2016-07-30 17:38:32 +0800

    feat: 支持指定返回1-5篇随机文章

--- script.py
+++ script.py
@@ -9,12 +9,18 @@ def queryData(query):
     issue = query
     if issue == 'l':
         params = {"type": "wanqu", "action": "getLatest"}
-    elif issue == 'r':
-        params = {"type": "wanqu", "action": "getRandom"}
+    elif re.match(r'^r(\d+)?$', issue, re.I):
+        matchedData = re.match(r'^r(\d+)?$', issue, re.I)
+        if issue == 'r':
+            params = {"type": "wanqu", "action": "getRandom"}
+        elif matchedData.group(1):
+            params = {"type": "wanqu", "action": "getRandom", "count": matchedData.group(1)}
+        else:
+            print "<items><item><title>输入的什么玩意?..</title><subtitle>正确的输入内容是字母l, 或者字母r, 或者r1类型的字母数字组合, 或者数字</subtitle><icon>icon.png</icon></item></items>"
     elif re.match(r'^\d$', issue, re.I):
         params = {"type": "wanqu", "action": "getSpec", "issue": issue}
     else:
-        print "<items><item><title>输入的什么玩意?..</title><subtitle>正确的输入内容是字母l, 或者字母r, 或者数字</subtitle><icon>icon.png</icon></item></items>"
+        print "<items><item><title>输入的什么玩意?..</title><subtitle>正确的输入内容是字母l, 或者字母r, 或者r1类型的字母数字组合, 或者数字</subtitle><icon>icon.png</icon></item></items>"
 
     data = urllib.urlencode(params)
     req = urllib2.Request('http://bigyoo.me/ns/cmd', data)
