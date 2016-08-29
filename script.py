1b1a270a9d60565f7a877b793bfe7c43d1b3e026
Mr.Krabs <pangxie.wdy@gmail.com>
2016-08-29 10:20:38 +0800

    refactor: 修改参数名

--- script.py
+++ script.py
@@ -19,17 +19,17 @@ def queryData(query):
     # 根据用户输入, 构造不同的数据请求
     issue = query
     if issue == 'l':
-        params = {"type": "wanqu", "action": "getLatest", "clientVersion": clientVersion, "macAddress": macAddress}
+        params = {"type": "wanqu", "action": "getLatest", "clientVersion": clientVersion, "mac": macAddress}
     elif re.match(r'^r(\d+)?$', issue, re.I):
         matchedData = re.match(r'^r(\d+)?$', issue, re.I)
         if issue == 'r':
-            params = {"type": "wanqu", "action": "getRandom", "clientVersion": clientVersion, "macAddress": macAddress}
+            params = {"type": "wanqu", "action": "getRandom", "clientVersion": clientVersion, "mac": macAddress}
         elif matchedData.group(1):
-            params = {"type": "wanqu", "action": "getRandom", "count": matchedData.group(1), "clientVersion": clientVersion, "macAddress": macAddress}
+            params = {"type": "wanqu", "action": "getRandom", "count": matchedData.group(1), "clientVersion": clientVersion, "mac": macAddress}
         else:
             print "<items><item><title>输入的什么玩意?..</title><subtitle>正确的输入内容是字母l, 或者字母r, 或者r1类型的字母数字组合, 或者数字</subtitle><icon>icon.png</icon></item></items>"
     elif re.match(r'^\d.*$', issue, re.I):
-        params = {"type": "wanqu", "action": "getSpec", "issue": issue, "clientVersion": clientVersion, "macAddress": macAddress}
+        params = {"type": "wanqu", "action": "getSpec", "issue": issue, "clientVersion": clientVersion, "mac": macAddress}
     else:
         print "<items><item><title>输入的什么玩意?..</title><subtitle>正确的输入内容是字母l, 或者字母r, 或者r1类型的字母数字组合, 或者数字</subtitle><icon>icon.png</icon></item></items>"
 
